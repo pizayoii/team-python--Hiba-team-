0f6c51c5c62ede56164a0e9e3bd459d567ae6d96
Basile Monier <basilemonier@gmail.com>
2026-03-31 20:00:25 +0200

    feat : add hightest and lowest expense display

--- depenses.py
+++ depenses.py
@@ -15,13 +15,18 @@ with open ("depenses.csv", "r", encoding="utf-8") as fichier :
             depense_loisirs.append(float(ligne["montant"]))
         elif ligne["categorie"] == "Transport" :
             depense_transport.append(float(ligne["montant"]))
+max_depense = max(total_depense)
+min_depense = min(total_depense)
 
-            
-        
+
+
 
 print("")
 print (f"La somme de toutes les dépenses est égal à : {sum(total_depense)}")
 print (f"La somme des dépenses dans l'alimentation est égal à : {sum(depense_alimentation)}.")
-print (f" La somme des dépenses dans les loisirs est égal à : {sum(depense_loisirs)}.")
+print (f"La somme des dépenses dans les loisirs est égal à : {sum(depense_loisirs)}.")
 print(f"La somme des dépenses dans le transport est égal à : {sum(depense_transport)}.")
-
+print ("")
+print ("")
+print (f"La plus grande dépenses est de : {max_depense}.")
+print (f"La plus petite dépenses est de : {min_depense}.")
