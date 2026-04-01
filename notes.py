b57715927a110dc3167b901196de5f326428a4bf
Izayoii <rakhisahmat3600@gmail.com>
2026-04-01 10:35:35 +0200

    feat: add average calculation fuction

--- notes.py
+++ notes.py
@@ -4,3 +4,7 @@ notes.extend([7, 14, 11])
 def afficher_notes(liste):
     print("Notes :", liste)
 afficher_notes(notes)
+def calculer_moyenne(liste):
+    return round(sum(liste) / len(liste), 1)
+
+print("Moyenne :", calculer_moyenne(notes))
