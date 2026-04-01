6c05585270b910bc8a30fab165064535ef8a2858
pikodes <valdestakou@gmail.com>
2026-04-01 08:42:02 +0100

    feat..-resultats

--- pythonteam.py
+++ pythonteam.py
@@ -13,12 +13,19 @@ with open("team.csv", "r", newline="", encoding="utf-8") as fichier:
            # tampon.append("points")  
             continue
         #print(f"this is {tampon[0]}")
-        equipe,victoires,nuls,defaites,points=line[0],line[1],line[2],line[3],[]
+        equipe,victoires,nuls,defaites,points=line[0],line[1],line[2],line[3],list()
         points.append(int(victoires)*3 +int(nuls))
         tampon.append([equipe,victoires,nuls,defaites,points])  
 
 with open("new_team.csv", "w", newline="", encoding="utf-8") as fichier:
     csv_writer=csv.writer(fichier)
-    csv_writer.writerows(tampon)        
+    csv_writer.writerows(tampon)  
+    print()      
+
+with open("new_team.csv", "r", encoding="utf-8") as fichier: 
+    lecteur = csv.DictReader(fichier) 
+    for ligne in lecteur: 
+        print(f"equipe: {ligne['equipe']}, victoires: {ligne['victoires']} , nuls: {ligne['nuls']} , defaites: {ligne['defaites']}, points: {ligne['points']}")     
 
 
+        
