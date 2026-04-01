7ab27925e1828b81ef018547b47d2c402b561b8e
Basile Monier <basilemonier@gmail.com>
2026-04-01 10:37:10 +0200

    feat: add total calculation function

--- scores.py
+++ scores.py
@@ -1,4 +1,11 @@
 scores = [250, 480, 310, 95, 720, 150, 888, 430]
 def afficher_scores(liste):
     print("Scores :", liste)
+
+
 afficher_scores(scores)
+def calculer_total(liste) :
+    somme = sum(liste)
+    print ("La somme est égal à :")
+    return somme
+print(calculer_total(scores))
