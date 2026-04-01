3b879e1ed33e655f8f1973a062eba73ed3dcb48b
hiba lamrichi <hibalamrichi4@gmail.com>
2026-04-01 11:13:51 +0200

    feat: add top 3 display function

--- scores.py
+++ scores.py
@@ -1,4 +1,24 @@
-scores = [250, 480, 310, 95, 720]
+scores = [250, 480, 310, 95, 720, 60, 487,165]
 def afficher_scores(liste):
     print("Scores :", liste)
+
 afficher_scores(scores)
+
+
+def afficher_top3(liste): 
+    scores_tries = sorted(liste, reverse=True) 
+    top_3 = scores_tries[:3] 
+    print(f"Top 3 scores :", top_3)
+    
+afficher_top3(scores)
+
+
+
+
+
+
+
+
+
+
+
