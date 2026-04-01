874a3fc6508e47801be14f9f339aff62e09e4f98
hiba lamrichi <hibalamrichi4@gmail.com>
2026-04-01 11:42:52 +0200

    feat: add level display function

--- scores.py
+++ scores.py
@@ -9,8 +9,21 @@ def afficher_top3(liste):
     scores_tries = sorted(liste, reverse=True) 
     top_3 = scores_tries[:3] 
     print(f"Top 3 scores :", top_3)
-    
+
 afficher_top3(scores)
+
+
+def afficher_niveau(liste):
+    for i in liste :
+        if i >= 700:
+         print(f"{i} = expert")
+        elif i >= 400:
+         print(f"{i} = avancé")
+        elif i < 400:
+         print(f"{i} = débutant")
+
+afficher_niveau(scores)
+
 
 
 
