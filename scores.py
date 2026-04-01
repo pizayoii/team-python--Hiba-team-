daf33952f4daf47bd94daaedbd3ff73fb99540ff
Basile Monier <basilemonier@gmail.com>
2026-04-01 10:53:55 +0200

    feat: add average calculation function

--- scores.py
+++ scores.py
@@ -2,10 +2,20 @@ scores = [250, 480, 310, 95, 720, 150, 888, 430]
 def afficher_scores(liste):
     print("Scores :", liste)
 
-
 afficher_scores(scores)
+
+
+
 def calculer_total(liste) :
     somme = sum(liste)
-    print ("La somme est égal à :")
+    print (f"La somme est égal à :", somme)
     return somme
 print(calculer_total(scores))
+
+
+
+def calculer_moyenne(liste) :
+    moyenne =  sum(liste) / len(liste)
+    print (f"La moyenne est égal à : ", round(moyenne, 2))
+    return moyenne
+print (calculer_moyenne(scores))
